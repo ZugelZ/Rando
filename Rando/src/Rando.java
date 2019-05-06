10a78f772f5117e1fd21728f2fb7a48c5e655695
Patrick Zugel <ZugelPK20@pwcs-edu.org>
2019-05-06 13:20:05 -0400

    I try to random but random no work! HELP!

---
+++ Rando/src/Rando.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Rando 
+{
+	public Rando()
+	{
+		random();
+	}
+	
+	public static void main( String[] args )
+	{
+		new Rando();
+	}
+	
+	/**
+	 * I swear to you that high is not inclusive, no no!
+	 * The priests have said on high that high is not to be included in the range!
+	 */
+	public void random()
+	{
+		Scanner scan = new Scanner( System.in );
+		int low = scan.next();
+		int high = scan.next();
+		System.out.println( Math.random() * (low + high) );
+	}
+}
