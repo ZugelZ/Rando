916d82bfdbfe745acad78258c200a0eed4eb154c
Kyle Rowland-Mungo <rowlandloud@gmail.com>
2019-05-06 13:46:18 -0400

    for the ethical touching of zugel

---
+++ task.java
@@ -0,0 +1,18 @@
+package taskofgit;
+import java.util.*;
+import java.lang.Math;
+public class task {
+
+	
+	public static void main(String[] args) {
+		System.out.println("enter two numbers moron");
+		Scanner s = new Scanner(System.in);
+		int ff = s.nextInt();
+		int f = s.nextInt();
+		System.out.println(((f+ff)/2) +""); 
+	
+	}
+	
+	
+	
+}
