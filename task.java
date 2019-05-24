ef8a470f1a47ec64e1de10ebd3b1dd1702c17621
Aaron Estrellado <aaron.estrellado@gmail.com>
2019-05-24 10:02:38 -0700

    Fixed minor mistakes

--- task.java
+++ task.java
@@ -1,9 +1,9 @@
 package taskofgit;
 import java.util.*;
-public class Task {
-
-	
-	public static void main(String[] args) {
+public class Task 
+{
+	public static void main(String[] args) 
+	{
 		System.out.println("enter two numbers, moron");
 		Scanner s = new Scanner(System.in);
 		int ff = s.nextInt();
@@ -11,7 +11,4 @@ public class Task {
 		System.out.println(((f+ff)/2) +""); 
 	
 	}
-	
-	
-	
 }
